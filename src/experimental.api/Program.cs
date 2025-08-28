b7f2affd74d52ec308f9148964aef6ac6970ae63
andrey-aka-skif <2119163@gmail.com>
2025-08-29 00:39:51 +0300

    Добавлены изменения для работы интеграционных тестов

--- src/experimental.api/Program.cs
+++ src/experimental.api/Program.cs
@@ -18,7 +18,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -36,3 +36,5 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+public partial class Program { }
